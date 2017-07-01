246ab141a4f39e13f4912d51436589246dbe68ab
Logan Graba <logan@wellcomemat.com>
2017-06-30 22:31:43 -0600

    Got the Server and Client communicating, as well as newBlock() being called with the genesis block and new txs

--- nutclient.py
+++ nutclient.py
@@ -6,15 +6,16 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 # Connect the socket to the port where the server is listening
 server_address = ('localhost', 10000)
-print("Connecting to {}:{}...".format(server_address))
+host, port = server_address
+print("Connecting to {}:{}...".format(host, port))
 sock.connect(server_address)
 
 try:
 	# Get and Send Data
-	message = raw_input('Please enter your message to send to the server:\n')
+	message = input('Please enter your message to send to the server:\n')
 	# message = 'This is a rather long message. I suppose it will be repeated. Logan is a pimp!'
 	print("Sending '{}'".format(message))
-	sock.sendall(message)
+	sock.sendall(message.encode('utf-8'))
 
 	# Gather Response
 	amount_received = 0
--- nutserver.py
+++ nutserver.py
@@ -8,42 +8,43 @@ class NutServer:
 	# Server Configuration Parameters
 	server_address = ('localhost', 10000)
 
-	def __init__():
+	def __init__(self):
 		# Create TCP/IP Socket
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 		# Bind socket to port
-		print("Starting up server...\nHost: {}\nPort: {}\n".format(server_address))
-		self.sock.bind(server_address)
+		host, port = self.server_address
+		print("Starting up server...\nHost: {}\nPort: {}\n".format(host, port))
+		self.sock.bind(self.server_address)
 
-	def startListening():
+	def startListening(self):
 		# Listen (1 connection at a time)
 		self.sock.listen(1)
 		# Any received transactions
 		transactions = {}
 
 		# Loop
-		while !transactions:
+		while not transactions:
 			# Wait for connection
 			print("Waiting for a connection...\n")
 			connection, client_address = self.sock.accept()
 
 			try:
-				print("Connection from ".format(client_address))
+				print("Connection from {}".format(client_address))
 
 				# Receive Data
-				while !transactions:
+				while not transactions:
 					data = connection.recv(16)
 					# Current UNIX timestamp
 					data_time = time.time()
-					print("Received '{}'".format(data)
+					print("Received '{}'".format(data))
 
 					if data:
 						print("Sending data back to client...\n")
 						connection.sendall(data)
 
 						# Test
-						test_data = "Give Sarah 10n from Logan at " + data_time + "; What a gal!"
+						test_data = "Give Sarah 10n from Logan at " + str(data_time) + "; What a gal!"
 						print("Submitting to NutChain")
 						transactions.update({1:test_data})
 					else:
